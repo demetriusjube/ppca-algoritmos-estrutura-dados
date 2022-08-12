03e56ea831c271c6c87e08461984007e2c4afee0
Demétrius de Almeida Jubé <jube@senado.leg.br>
2022-08-12 10:07:11 -0300

    Algoritmo de permutação

---
+++ algoritmo-caminho-grafo.py
@@ -0,0 +1,19 @@
+from sre_constants import IN
+
+
+INF = 999999
+DIST_NULA = '0'
+COLUNAS_PERMUTACAO = [1,2,3]
+matriz = [[DIST_NULA, 5, 3, 1], 
+    [5, DIST_NULA, INF, 4],
+    [3, INF, DIST_NULA, 2],
+    [1, 4, 2, DIST_NULA]]
+
+
+for i in COLUNAS_PERMUTACAO:
+    for j in COLUNAS_PERMUTACAO:
+        for k in COLUNAS_PERMUTACAO:
+            if (i!= j and j != k and i != k):
+                custo = matriz[0][i] + matriz[i][j] + matriz[j][k] + matriz[k][0]
+                print('Custo do caminho ','0', i, j, k, '0', ': ', custo)
+

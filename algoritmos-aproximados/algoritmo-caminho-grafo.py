c0f36a540a3bd19aaa366f5c08fe3ae9e591b1d2
Demétrius de Almeida Jubé <jube@senado.leg.br>
2022-08-12 10:56:48 -0300

    Alteração dos valores

--- algoritmos-aproximados/algoritmo-caminho-grafo.py
+++ algoritmos-aproximados/algoritmo-caminho-grafo.py
@@ -1,19 +1,26 @@
 from sre_constants import IN
 
 
-INF = 2
+INF = 999999
 DIST_NULA = '0'
 COLUNAS_PERMUTACAO = [1,2,3]
 matriz = [[DIST_NULA, 5, 3, 1], 
-    [5, DIST_NULA, 10, 4],
-    [3, 10, DIST_NULA, 2],
+    [5, DIST_NULA, 9, 4],
+    [3, 9, DIST_NULA, 2],
     [1, 4, 2, DIST_NULA]]
 
+menor_custo = INF 
+menor_caminho = ''
 
 for i in COLUNAS_PERMUTACAO:
     for j in COLUNAS_PERMUTACAO:
         for k in COLUNAS_PERMUTACAO:
             if (i!= j and j != k and i != k):
                 custo = matriz[0][i] + matriz[i][j] + matriz[j][k] + matriz[k][0]
-                print('Custo do caminho ','0', i, j, k, '0', ': ', custo)
+                caminho = '0'+ str(i) + str(j) +str(k) +'0'
+                if (custo < menor_custo):
+                    menor_custo = custo
+                    menor_caminho = caminho
+                print('Custo do caminho ',caminho, ': ', custo)
 
+print('Menor ciclo: ', menor_caminho, ' com custo ', menor_custo)

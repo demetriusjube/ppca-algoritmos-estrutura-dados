7038a3fa7938ba37b8f0f715b2585d1ec7d1fac1
Demétrius de Almeida Jubé <jube@senado.leg.br>
2022-09-22 01:00:22 -0300

    Classe Stack

--- exercicio-2/classe-stack.py
+++ exercicio-2/classe-stack.py
@@ -17,3 +17,37 @@
 # True
 # (]
 # False
+
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+    
+    def size(self):
+        return len(self.data)
+
+def isSimbolosBalanceados(expressao):
+    stack = Stack()
+    
+    for caracter in expressao:
+        if caracter == '(' or caracter == '[' or caracter == '{':
+            stack.push(caracter)
+        else:
+            ultimoElemento = stack.data[-1]
+            if (caracter == ")" and ultimoElemento == "(") or (caracter == "]" and ultimoElemento == "[") or (caracter == "}" and ultimoElemento == "{"):
+                stack.pop()
+            else:
+                return False
+        
+    return stack.isEmpty()
+
+x = input()
+print(isSimbolosBalanceados(x))

b796191b6b5afca3062b6baa6e222018bd1684af
Demétrius de Almeida Jubé <jube@senado.leg.br>
2022-09-22 00:25:17 -0300

    Resolução fibonnacci

--- exercicio-2/fibonnacci.py
+++ exercicio-2/fibonnacci.py
@@ -9,3 +9,20 @@
 # Input	Result
 # 10
 # 55
+
+
+def fibonacciList(valor):
+    if (valor <= 2):
+        return 1
+    else:
+        lista = [0,1]
+        for i in range(valor-1):
+            f_n=lista[0]
+            f_n_1 = lista[1]
+            f_n_2 = f_n + f_n_1
+            lista.pop(0)
+            lista.append(f_n_2)
+        return lista[1]
+
+valor = int(input())
+print(fibonacciList(valor))
